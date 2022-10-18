972adc0cef90ba1407f38e8a63aa6139ae212ff5
James Wilcock <jameswilcock8@gmail.com>
2022-10-17 21:55:26 -0700

    added logging and fix for when new db same name as existing

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sqlite3 as sql
+import logging
 import argparse
 
+logging.basicConfig(format='%(asctime)s - %(name)s:%(levelname)s:%(message)s', level=logging.DEBUG)
 ap = argparse.ArgumentParser()
 ap.add_argument("-n", "--new", required=False, help="creates a new db with given name")
 ap.add_argument("-o", "--open", required=False, help="opens existing db for viewing or editing")
@@ -13,8 +15,10 @@ if not args['new'] and not  args['open']:
 if args['new']:
     con = sql.connect(f"{args['new']}")
     cur = con.cursor()
-    cur.execute("CREATE TABLE items(category, item, quantity, location)")
+    logging.info(f"Connected to db {args['new']}")
+    cur.execute("CREATE TABLE IF NOT EXISTS items(category, item, quantity, location)")
     con.commit()
 else:
     con = sql.connect(f"{args['open']}")
+    logging.info(f"Connected to db {args['open']}")
     cur = con.cursor()
