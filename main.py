920d6ee70cca01ed7a9cd703caaa62c21a734905
AineeJames <olsenaiden33@gmail.com>
2022-10-17 21:41:36 -0700

    added db

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-import sqlite3
+import sqlite3 as sql
+import argparse
 
-print("ello werld")
+ap = argparse.ArgumentParser()
+ap.add_argument("-n", "--new", required=False, help="creates a new db with given name")
+ap.add_argument("-o", "--open", required=False, help="opens existing db for viewing or editing")
+args = vars(ap.parse_args())
+
+if args['new']:
+    con = sql.connect(f"{args['new']}")
+    cur = con.cursor()
+    cur.execute("CREATE TABLE categories(category, itemid)")
+    con.commit()
+else:
+    con = sql.connect(f"{args['open']}")
+    cur = con.cursor()
