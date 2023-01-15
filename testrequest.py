e65bba0f68e9a8fec9bc20d8a55fa11c9b895d21
James Wilcock <jameswilcock8@gmail.com>
2023-01-14 19:49:35 -0800

    slowly getting there

--- testrequest.py
+++ testrequest.py
@@ -5,13 +5,18 @@ r = requests.get("https://www.alldatasheet.com/view.jsp?Searchword=LM324")
 
 soup = BeautifulSoup(r.content, "html.parser")
 
-print(soup.prettify())
-s = soup.find_all("table", class_="width100yo")
-print(s[0])
+soup = soup.find_all("div", class_="overflowyo")[2]
+s = soup.find_all("table", class_="main")
+rowlist = []
+counter = 0
 for row in s[0].find_all("tr"):
     cells = row.find_all("td")
+    rowlist.append([])
     for cell in cells:
         a = cell.find_all("a")
         if len(a) > 0:
-            print(cell)
-            print(a[0]["href"][2:])
+            rowlist[counter].append(a[0]["href"][2:])
+        else:
+            rowlist[counter].append(cell.text)
+    counter += 1
+print(rowlist)
